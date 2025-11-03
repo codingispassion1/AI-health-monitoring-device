e57c6b45c183979fdba7b4b504311e4e8dfc819c
Aniruddh sharma <167106799+codingispassion1@users.noreply.github.com>
2025-11-03 13:12:10 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,14 +1,1 @@
-from flask import Flask, request, jsonify
-from model import check_anomaly
 
-app = Flask(__name__)
-
-@app.route("/analyze", methods=["POST"])
-def analyze():
-    data = request.json
-    bpm, spo2 = data["bpm"], data["spo2"]
-    status = check_anomaly(bpm, spo2)
-    return jsonify({"status": status})
-
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
